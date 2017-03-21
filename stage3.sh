7d59d44821d83bc20e01a2dbc737b17324d4623b
Thomas Herr <thom1810@gmail.com>
2017-03-21 18:43:48 -0400

    Starting working on second assignment

---
+++ stage3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Thomas Herr (200325519)
+
+echo "COMP2018-17W-21160 Research Assignment Stage 3";
+echo "Submission: Thomas Herr (200325519)";
+
+echo "1)";
+date '+%A';
+
+echo "2)";
+echo "It is illegal to end an RE with '\'.";
+
+echo "3)";
+echo "locate uses a database that is updated when programs install/uninstall etc
+ however it does not track or search for files in real time so can be inaccurate. Whereis finds binaries and manual pages by name";
+
+echo "4)";
+mkdir $HOME/tmp;
+
+
+
