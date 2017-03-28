f75becac58f129e3979e4a5ac254cb45cf6d2bb5
Thomas Herr <thom1810@gmail.com>
2017-03-28 12:19:15 -0400

    Finished Question six

--- stage3.sh
+++ stage3.sh
@@ -24,6 +24,7 @@ echo "locate uses a database that is updated when programs install/uninstall etc
 
 echo "4)";
 mkdir $HOME/tmp;
+ls -la $HOME | grep tmp;
 
 # Copy files from generated into tmp, only copy files that have a single character in their name
 echo "5)";
@@ -31,5 +32,24 @@ echo "5)";
 cd filetest;
 find . -name '?' -type f -exec rsync -Raq {} ~/tmp \;
 cd ../;
+
+# In your tmp directory (not in any subdirectories), rename the files whose names are numbers to be those numbers repeated twice (e.g. a file named 0 becomes a file named 00). Do a recursive listing of your tmp directory to show what is there now.
+echo "6)";
+find $HOME/tmp -maxdepth 1 -name '?' -type f;
+
+find $HOME/tmp -maxdepth 1 -name '?' -type f 
+-exec bash -c 'mv -v $0 ${0}$(echo -n $0 | tail -c 1)' {} \;
+
+find $HOME/tmp -maxdepth 1 -name '??' -type f;
+
+
+
+
+
+
+
+
+
+
 
 
