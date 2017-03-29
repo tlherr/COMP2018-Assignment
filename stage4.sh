516ae745eecf48d6afdb25d4d326013967b3f9c4
Thomas Herr <thom1810@gmail.com>
2017-03-28 21:01:33 -0400

    Working on stage 4 question 3

--- stage4.sh
+++ stage4.sh
@@ -18,5 +18,24 @@ ls -laR "$HOME/tmp2";
 # Use gzip to create a gzip file containing the contents of your tmp directory tree. Use the table of contents option to gzip to display a listing of the files in your newly created gzip file. Make a new directory named tmp3 in your home directory. In that directory extract only the b subdirectory along with its files from your gzip file. Do a recursive long listing of your tmp3 directory to show what got restored from your gzip file.
 
 echo "2)";
+tar -czf tmp2archive.tar.gz;
+
+mkdir "$HOME/tmp3";
+
+tar -z --extract --file=tmp2archive.tar.gz b --directory "$HOME/tmp3";
+
+ls -laR "$HOME/tmp3";
+
+# Use zip to create a zip file containing the contents of your tmp directory tree. Use the table of contents option to zip to display a listing of the files in your newly created zip file. Make a new directory named tmp4 in your home directory. In that directory extract only the c subdirectory along with its files from your zip file. Do a recursive long listing of your tmp4 directory to show what got restored from your zip file.
+
+echo "3)";
+
+
+
+
+
+
+
+
 
 
